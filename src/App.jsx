b66ee56230c1a55cf24e80c9e277b5c261140d80
Dhruv Gupta <125383912+dhruvgupta114@users.noreply.github.com>
2024-04-29 23:23:44 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { ThemeProvider } from "./context/Theme";
+import Card from "./components/Card";
+import Theme from "./components/Theme";
+
+function App() {
+  const [themeMode, setThemeMode] = useState("light")
+
+  const lightTheme = () =>{
+    setThemeMode("light")
+  }
+  
+  const darkTheme = () =>{
+    setThemeMode("dark")
+  }
+
+  useEffect(()=>{
+    document.querySelector('html').classList.remove('light', 'dark')
+    document.querySelector('html').classList.add(themeMode)
+  },[themeMode])
+  
+
+  return (
+    <ThemeProvider value={{darkTheme, lightTheme, themeMode}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <Theme/>
+          </div>
+          <div className="w-full max-w-sm mx-auto">
+            <Card/>
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
